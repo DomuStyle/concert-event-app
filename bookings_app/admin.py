cfbfcbea185305eec8250bfe361fbefcc5039d51
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-22 19:12:57 +0200

    added: BookingAdmin + readonly_fields

--- bookings_app/admin.py
+++ bookings_app/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Participant, Booking
 # Register your models here.
 
+
+class BookingAdmin(admin.ModelAdmin):
+    # make the booking_date field read-only in the add/edit forms, as it’s auto-set on creation.
+    readonly_fields = ('booking_date')
+
 admin.site.register(Participant)
 admin.site.register(Booking)
