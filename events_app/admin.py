be96ed3e610db47286e66f6dffabc515106bf005
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-22 16:35:54 +0200

    added: search_fields & list_filter

--- events_app/admin.py
+++ events_app/admin.py
@@ -4,8 +4,15 @@ from .models import EventCategory, Event, Location
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-    # Specify the fields to display in the admin list view for Events.
+
+    # specify the fields to display in the admin list view for Events.
     list_display = ("title", "category", "location", "date")
+
+    # define fields that can be searched in the admin search bar.
+    search_fields = ("title", "date")
+    
+    # Add filters to the admin list view, allowing filtering by category.
+    list_filter = ["category"]
 
 
 admin.site.register(EventCategory)
