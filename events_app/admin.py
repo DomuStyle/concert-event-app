321425ccfe84f1e4f32ffa11aa70d2a928c168a0
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-22 16:14:32 +0200

    fixed: model migration

--- events_app/admin.py
+++ events_app/admin.py
@@ -3,6 +3,11 @@ from .models import EventCategory, Event, Location
 
 # Register your models here.
 
+class EventAdmin(admin.ModelAdmin):
+    # Specify the fields to display in the admin list view for Events.
+    list_display = ("title", "category", "location", "date")
+
+
 admin.site.register(EventCategory)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(Location)
